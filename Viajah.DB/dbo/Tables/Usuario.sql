6c3b981877b2ea6c8d994a5ea3b723bd4c467fff
Guilherme Simão Costa <Gui_SC@outlook.com>
2018-11-08 11:23:28 -0200

    Essenciais

---
+++ Viajah.DB/dbo/Tables/Usuario.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Usuario]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Login] NVARCHAR(50) NULL, 
+    [Senha] NVARCHAR(50) NULL
+)
---
+++ Viajah.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Viajah.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
